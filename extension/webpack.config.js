afb14879528bc1429db66c8e4943e255f7f46628
Chiawen Chen <golopot@gmail.com>
2022-04-16 16:06:14 +0800

    Configure webpack terser

--- extension/webpack.config.js
+++ extension/webpack.config.js
@@ -1,4 +1,6 @@
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const WebpackPrettierPlugin = require('webpack-prettier-plugin');
 
 module.exports = {
   entry: './main.js',
@@ -11,6 +13,19 @@ module.exports = {
     new CopyWebpackPlugin({
       patterns: [{from: 'public'}],
     }),
+    new WebpackPrettierPlugin(),
   ],
-  mode: 'production',
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          ecma: 2018,
+          compress: false,
+          mangle: false,
+        },
+      }),
+    ],
+  },
+  mode: 'none',
 };
